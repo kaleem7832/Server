d36a19a51115d99cb212d04b28a84fcbe1ba85c6
kaleem7832 <39048476+kaleem7832@users.noreply.github.com>
2024-01-31 14:55:54 +0530

    Update DB.js
    Syntax error fixed

--- DB.js
+++ DB.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB:
-    ""mongodb+srv://kaleem:1234@cluster0.egghxjo.mongodb.net/Tasks?retryWrites=true&w=majority",
+    "mongodb+srv://kaleem:1234@cluster0.egghxjo.mongodb.net/Tasks?retryWrites=true&w=majority"
 };
