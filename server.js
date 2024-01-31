57b8e642fafdf5d25ad379f7f4d0ce42e10db2f7
kaleem7832 <39048476+kaleem7832@users.noreply.github.com>
2024-01-31 17:02:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -96,9 +96,9 @@ app.use("/tasks", taskRoutes);
 //   });
 // });
 // Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+//app.get("*", (req, res) => {
+//  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//});
 app.listen(PORT, function () {
   console.log("Server is running on Port:", PORT);
 });
